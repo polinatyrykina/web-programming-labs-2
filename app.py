3c56aacb4bcdc47523750e805bf643db43a66a12
Тырыкина Полина <polinatyrykina@gmail.com>
2024-09-09 12:10:54 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-    return """
+    return redirect("/menu", code=302)
+
+@app.route("/menu")
+def menu():
+    return"""
 <!DOCTYPE html>
 <html>
     <head>
