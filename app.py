40222e33e9117c8cb5aae6545d9f3219cdfe906f
Тырыкина Полина <polinatyrykina@gmail.com>
2024-09-09 09:35:06 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Тырыкина Полина Анатольевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
 
- 
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+                &copy; Тырыкина Полина, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
