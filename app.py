37dbc53cea3f5ff8537922f2ae653205fe14e890
Тырыкина Полина <polinatyrykina@gmail.com>
2024-10-03 19:42:54 +0700

    Книги

--- app.py
+++ app.py
@@ -14,8 +14,19 @@ def example():
         {'name':'мандарины', 'price':85},
         {'name':'манго', 'price':321}
     ]
+    books = [
+        {'name':'Зверобой', 'author': 'Купер Джеймс Фенимор', 'genre':'Детская художественная литература', 'count':480},
+        {'name':'Шанхайская головоломка', 'author': 'Чэнь Ши', 'genre':'Детективный роман', 'count':288},
+        {'name':'Сюжет', 'author': 'Корелиц Джин', 'genre':'Детективный роман', 'count':480},
+        {'name':'Сказать жизни "ДА!": психолог в концлагере', 'author': 'Франкл Виктор', 'genre':'Психология личности.', 'count':239},
+        {'name':'Дом, в котором...', 'author': 'Петросян Мариам', 'genre':'Современная проза', 'count':944},
+        {'name':'Мара и Морок. Трилогия', 'author': 'Арден Лия', 'genre':'Фэнтези', 'count':800},
+        {'name':'Ночь морлоков', 'author': 'Джетер Кевин Уэйн', 'genre':'Научная фантастика', 'count':320},
+        {'name':'Семья для чемпиона', 'author': 'Коваль Алекс', 'genre':'Любовные романы', 'count':480},
+        {'name':'Клинок, рассекающий демонов. Том 1 ', 'author': 'Готогэ Коёхару', 'genre':'Манга', 'count':192},
+    ]
     return render_template('example.html', name=name, lab_num=lab_num, 
-                           group=group, course=course, fruits=fruits)
+                           group=group, course=course, fruits=fruits, books=books)
 
 @app.route("/")
 @app.route("/index")
