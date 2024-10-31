adea80928c54bf1f1f06fbdc6854b55d6212b920
Тырыкина Полина <polinatyrykina@gmail.com>
2024-10-31 19:52:29 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -23,3 +23,75 @@ def div():
     x2 = int(x2) 
     result = x1 / x2 
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('/lab4/sum-form.html')
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum_function():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    # Преобразуем в 0, если поля пустые
+    x1 = int(x1) if x1 else 0
+    x2 = int(x2) if x2 else 0
+    
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('/lab4/mul-form.html')
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul_function():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    # Преобразуем в 1, если поля пустые
+    x1 = int(x1) if x1 else 1
+    x2 = int(x2) if x2 else 1
+
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('/lab4/sub-form.html')
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub_function():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('/lab4/pow-form.html')
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def pow_function():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/pow.html', error1='Невозможно возвести 0 в 0.')
+    
+    result = x1 ** x2
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
