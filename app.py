21da3820d8791308aba1a66b99779e9c3a2e7861
Тырыкина Полина <polinatyrykina@gmail.com>
2024-11-14 22:21:33 +0700

    Снова правки

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, session
+from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
