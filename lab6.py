4cfabc30569fe4d3e1f07be4cd6bb9522c23fbcd
Тырыкина Полина <polinatyrykina@gmail.com>
2024-11-16 14:50:39 +0700

    Цена у кабинетов

--- lab6.py
+++ lab6.py
@@ -3,7 +3,7 @@ lab6 = Blueprint('lab6', __name__)
 
 offices = []
 for i in range(1,11):
-    offices.append({"number": i, "tenant": ""})
+    offices.append({"number": i, "tenant": "", "price" : 900 + i%8})
 
 
 @lab6.route('/lab6/')
