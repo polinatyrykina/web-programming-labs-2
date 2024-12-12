18fa181e2f5cebddec322298e67b5aecad457fa0
Тырыкина Полина <polinatyrykina@gmail.com>
2024-12-12 21:56:00 +0700

    Настройка хостинга

--- lab7.py
+++ lab7.py
@@ -9,27 +9,27 @@ lab7 = Blueprint('lab7', __name__)
 
 
 def db_connect():
-    conn = psycopg2.connect(
-        host='127.0.0.1',
-        database='polina_tyrykina_knowledge_base',
-        user='polina_tyrykina_knowledge_base',
-        password='123'
-    )
-    cur = conn.cursor(cursor_factory=RealDictCursor)
+    if current_app.config['DB_TYPE']=='postgres':
+        conn=psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'polina_tyrykina_knowledge_base',
+            user = 'polina_tyrykina_knowledge_base',
+            password = '123',
+            port=5432
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
     return conn, cur
 
 def db_close(conn, cur):
     conn.commit()
     cur.close()
     conn.close()
-
-
-def db_close(conn, cur):
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
 
 @lab7.route('/lab7/')
 def main():
@@ -94,33 +94,38 @@ def main():
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
     conn, cur = db_connect()
-    cur.execute("SELECT * FROM films;")
+    cur.execute("SELECT * FROM films")
     films = cur.fetchall()
     db_close(conn, cur)
-    return jsonify(films)
+    return jsonify([dict(film) for film in films])
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
     conn, cur = db_connect()
-    cur.execute("SELECT * FROM films WHERE id = %s;", (id,))
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("SELECT * FROM films WHERE id = ?;", (id,))
     film = cur.fetchone()
     db_close(conn, cur)
     if not film:
         abort(404, description="Фильм не найден")
-    return jsonify(film)
+    return jsonify(dict(film))
 
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def delete_film(id):
     conn, cur = db_connect()
-    cur.execute("DELETE FROM films WHERE id = %s;", (id,))
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("DELETE FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("DELETE FROM films WHERE id = ?;", (id,))
     db_close(conn, cur)
     return '', 204
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
 def update_film(id):
-    """Обновить информацию о фильме"""
     film = request.get_json()
     title = film.get('title')
     title_ru = film.get('title_ru')
@@ -140,12 +145,17 @@ def update_film(id):
         return jsonify({'error': 'Год выпуска должен быть между 1895 и 2024'}), 400
 
     conn, cur = db_connect()
-    cur.execute(
-        "UPDATE films SET title = %s, title_ru = %s, year = %s, description = %s WHERE id = %s;",
-        (title, title_ru, year, description, id)
-    )
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(
+            "UPDATE films SET title = %s, title_ru = %s, year = %s, description = %s WHERE id = %s;",
+            (title, title_ru, year, description, id)
+        )
+    else:
+        cur.execute(
+            "UPDATE films SET title = ?, title_ru = ?, year = ?, description = ? WHERE id = ?;",
+            (title, title_ru, year, description, id)
+        )
     db_close(conn, cur)
-
     return jsonify({'id': id}), 200
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
@@ -173,11 +183,18 @@ def add_film():
         return jsonify(errors), 400
 
     conn, cur = db_connect()
-    cur.execute(
-        "INSERT INTO films (title, title_ru, year, description) VALUES (%s, %s, %s, %s) RETURNING id;",
-        (title, title_ru, year, description)
-    )
-    film_id = cur.fetchone()['id']
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute(
+            "INSERT INTO films (title, title_ru, year, description) VALUES (%s, %s, %s, %s) RETURNING id;",
+            (title, title_ru, year, description)
+        )
+    else:
+        cur.execute(
+            "INSERT INTO films (title, title_ru, year, description) VALUES (?, ?, ?, ?);",
+            (title, title_ru, year, description)
+        )
+        cur.execute("SELECT last_insert_rowid();")
+    film_id = cur.fetchone()['id'] if current_app.config['DB_TYPE'] == 'postgres' else cur.fetchone()[0]
     db_close(conn, cur)
 
     return jsonify({'id': film_id}), 201
