041d3965baa46e1dde0e9f22aa23d71ac13eab93
Тырыкина Полина <polinatyrykina@gmail.com>
2024-12-22 15:52:31 +0700

    hup

--- lab8.py
+++ lab8.py
@@ -1,25 +1,25 @@
-import os
-from flask import Blueprint, render_template, request, jsonify, abort, session, current_app
-import psycopg2
-import sqlite3
-from os import path
-from psycopg2.extras import RealDictCursor
+# import os
+# from flask import Blueprint, render_template, request, jsonify, abort, session, current_app
+# import psycopg2
+# import sqlite3
+# from os import path
+# from psycopg2.extras import RealDictCursor
 
-lab8 = Blueprint('lab8', __name__)
+# lab8 = Blueprint('lab8', __name__)
 
-@lab8.route('/lab8/')
-def lab():
-    return render_template('lab8/lab8.html')
+# @lab8.route('/lab8/')
+# def lab():
+#     return render_template('lab8/lab8.html')
 
-@lab8.route('/lab8/register')
-def register():
-        return render_template('lab8/register.html')
+# @lab8.route('/lab8/register')
+# def register():
+#         return render_template('lab8/register.html')
 
-@lab8.route('/lab8/articles') 
-def article(): 
-        return render_template('lab8/articles.html')
+# @lab8.route('/lab8/articles') 
+# def article(): 
+#         return render_template('lab8/articles.html')
 
-@lab8.route('/lab8/create') 
-def create(): 
-        return render_template('lab8/create_article.html')
+# @lab8.route('/lab8/create') 
+# def create(): 
+#         return render_template('lab8/create_article.html')
 
