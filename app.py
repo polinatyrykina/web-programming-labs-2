71dfdcdb401f3aff3bd84c44ce504d515e51205b
Тырыкина Полина <polinatyrykina@gmail.com>
2024-12-22 22:48:28 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
-# from lab8 import lab8
+from lab8 import lab8
 from rgz import rgz
 
 
 import os
+from os import path
 from dotenv import load_dotenv
-# from flask_sqlalchemy import SQLAlchemy
-# from db import db
+from db import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -22,6 +23,21 @@ load_dotenv()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'polina_tyrykina_orm'
+    db_user = 'polina_tyrykina_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "polina_tyrykina_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite://{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -30,7 +46,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
-# app.register_blueprint(lab8)
+app.register_blueprint(lab8)
 app.register_blueprint(rgz)
 
 @app.route("/")
